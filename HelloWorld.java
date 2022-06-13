210f1a104b665ef5670c5a052071a170c862e724
Jayesh Sonawane <jayeshs7744@gmail.com>
2022-06-13 20:04:30 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.Demo_Project;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello");
-
-    }
-}
