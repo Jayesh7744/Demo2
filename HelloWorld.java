f5dbdc074c4ed54a90957d01685c1e72197e32f2
Jayesh7744 <Jayeshs7744@gmail.com>
2022-06-13 20:03:51 +0530

    Added: gitignore

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.Demo_Project;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello");
+
+    }
+}
